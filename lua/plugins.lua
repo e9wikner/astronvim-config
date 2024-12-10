4b95599f64a12e2afe58b3464e55e06048977c44
Stefan Wikner <stefan.wikner@volvocars.com>
2024-12-10 14:16:01 +0100

    Add CopilotChat and some more plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,5 +4,29 @@ return {
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "plugins" },
   },
+  {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "plugins" },
+  },
+  {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
+    { import = "plugins" },
+  },
   { "github/copilot.vim" },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
