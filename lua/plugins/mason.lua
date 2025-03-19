c318773505e3a365e2fd88925391975c610be5e6
Stefan Wikner <stefan.wikner@volvocars.com>
2025-03-19 14:06:12 +0100

    Configure formatters with Mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Mason plugins
 
@@ -12,6 +12,7 @@ return {
       ensure_installed = {
         "lua_ls",
         -- add more arguments for adding more language servers
+        "clangd",
       },
     },
   },
@@ -23,6 +24,8 @@ return {
       ensure_installed = {
         "stylua",
         -- add more arguments for adding more null-ls sources
+        "black",
+        "clang-format",
       },
     },
   },
