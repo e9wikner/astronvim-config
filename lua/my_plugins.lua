118c02b77343b2541c993c44579bceed1e11dac3
Stefan Wikner <stefan.wikner@volvocars.com>
2025-04-07 12:42:40 +0200

    Configure copilot chat

--- lua/my_plugins.lua
+++ lua/my_plugins.lua
@@ -9,9 +9,16 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      debug = true, -- Enable debugging
       -- See Configuration section for rest
     },
     -- See Commands section for default commands if you want to lazy load on them
+    keys = {
+      { "<leader>zc", "<cmd>CopilotChat<cr>", desc = "Copilot Chat" },
+      { "<leader>zC", "<cmd>CopilotChat --context<cr>", desc = "Copilot Chat" },
+      { "<leader>zr", "<cmd>CopilotChatReview<cr>", mode = "v", desc = "Copilot Chat Review" },
+      { "<leader>zo", "<cmd>CopilotChatOptimize<cr>", mode = "v", desc = "Copilot Chat Optimize" },
+      { "<leader>zf", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "Copilot Chat Fix" },
+      { "<leader>zd", "<cmd>CopilotChatDocs<cr>", mode = "v", desc = "Copilot Chat Docs" },
+    },
   },
 }
